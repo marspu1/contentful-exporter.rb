d4a9a1e2442abc5e37b537f35e7a087ecb62ce3c
Nick Tzaperas <nick@nijotz.com>
2015-05-13 17:40:31 -0700

    Test export and then import

--- contentful_exporter.gemspec
+++ contentful_exporter.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.6'
   spec.add_development_dependency 'rake'
 
+  spec.add_development_dependency 'contentful-importer'
   spec.add_development_dependency 'rspec', '~> 3'
   spec.add_development_dependency 'rspec-its', '~> 1.1.0'
   spec.add_development_dependency 'vcr', '~> 2.9.3'
--- spec/lib/migrator_spec.rb
+++ spec/lib/migrator_spec.rb
@@ -1,12 +1,44 @@
 require 'spec_helper'
 require './lib/contentful/exporter/migrator'
 
+def clear_space(space_id, access_token)
+  require 'contentful/management'
+  Contentful::Management::Client.new(access_token, raise_errors: true, logger: Logger.new(STDOUT), log_level: Logger::WARN)
+  space = Contentful::Management::Space.find(space_id)
+
+  space.entries.all(limit: 1000).each do |e|
+    e.unpublish if e.published?
+    e.destroy
+    puts "Cleared entry: #{e.id} (#{e.name rescue ''})"
+  end
+
+  space.content_types.all.each do |ct|
+    ct.deactivate if ct.active?
+    ct.destroy
+    puts "Cleared content type: #{ct.id} (#{ct.name rescue ''})"
+  end
+
+  space.assets.all.each do |a|
+    a.unpublish if a.published?
+    a.destroy
+    puts "Cleared asset: #{a.id} (#{a.name rescue ''})"
+  end
+end
+
 module Contentful
   module Exporter
     describe Migrator do
+
       before do
         @settings_file = YAML.load_file('spec/fixtures/settings/settings.yml')
       end
+
+      it 'export all from Contentful' do
+        vcr('export') do
+          Migrator.new(@settings_file).run('--export')
+        end
+      end
+
       it 'create content type json files from contentful structure' do
         vcr('export_content_types') do
           Migrator.new(@settings_file).run('--export-content-types')
@@ -26,9 +58,27 @@ module Contentful
         end
       end
 
-      it 'export all from Contentful' do
+      it 'can import what it exports' do
+        require 'contentful/importer/migrator'
+
+        import_settings = {
+          'data_dir' => @settings_file['data_dir'],
+          'space_id' => @settings_file['import_space_id'],
+          'access_token' => @settings_file['import_access_token'],
+        }
+        importer = Contentful::Importer::Migrator.new(import_settings)
+
         vcr('export') do
-          Migrator.new(@setting_file).run('--export')
+          Migrator.new(@settings_file).run('--export')
+        end
+
+        vcr('import') do
+          clear_space(@settings_file['import_space_id'], @settings_file['import_access_token'])
+          importer.run('--import-content-types', space_id: @settings_file['import_space_id'])
+          importer.run('--import-assets')
+          importer.run('--publish-assets', threads: 2)
+          importer.run('--import', threads: 2)
+          importer.run('--publish-entries', threads: 2)
         end
       end
 
