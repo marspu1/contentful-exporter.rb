b4dbaa6864a837fd08292fe55a7df3e1fce891e9
Nick Tzaperas <nick@nijotz.com>
2015-05-13 17:37:52 -0700

    setting_file -> settings_file

--- spec/lib/migrator_spec.rb
+++ spec/lib/migrator_spec.rb
@@ -5,24 +5,24 @@ module Contentful
   module Exporter
     describe Migrator do
       before do
-        @setting_file = YAML.load_file('spec/fixtures/settings/settings.yml')
+        @settings_file = YAML.load_file('spec/fixtures/settings/settings.yml')
       end
       it 'create content type json files from contentful structure' do
         vcr('export_content_types') do
-          Migrator.new(@setting_file).run('--export-content-types')
+          Migrator.new(@settings_file).run('--export-content-types')
         end
       end
 
       it 'export entires from Contentful with two Threads' do
         vcr('export_entries') do
           allow(FileUtils).to receive(:rm_r)
-          Migrator.new(@setting_file).run('--export-entries', threads: 2)
+          Migrator.new(@settings_file).run('--export-entries', threads: 2)
         end
       end
 
       it 'export an assets from Contentful' do
         vcr('export_assets') do
-          Migrator.new(@setting_file).run('--export-assets')
+          Migrator.new(@settings_file).run('--export-assets')
         end
       end
 
@@ -36,14 +36,14 @@ module Contentful
         it 'when valid' do
           vcr('valid_credentials') do
             expect_any_instance_of(Logger).to receive(:info).with('Contentful Management API credentials: OK')
-            Migrator.new(@setting_file).run('--test-credentials')
+            Migrator.new(@settings_file).run('--test-credentials')
           end
         end
         it 'when invalid' do
           vcr('invalid_credentials') do
             expect_any_instance_of(Logger).to receive(:info).with('Contentful Management API credentials: INVALID (check README)')
-            @setting_file['access_token'] = 'bad'
-            Migrator.new(@setting_file).run('--test-credentials')
+            @settings_file['access_token'] = 'bad'
+            Migrator.new(@settings_file).run('--test-credentials')
           end
         end
       end
