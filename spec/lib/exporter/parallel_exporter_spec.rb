81da3d9b22c892eb4c4cddf76f8db2167c654e35
Nick Tzaperas <nick@nijotz.com>
2015-07-09 11:30:54 -0700

    Updated tests

--- spec/lib/exporter/parallel_exporter_spec.rb
+++ spec/lib/exporter/parallel_exporter_spec.rb
@@ -12,40 +12,6 @@ module Contentful
         @exporter = ParallelExporter.new(@config)
       end
 
-      it 'number of threads' do
-        number = @exporter.number_of_threads
-        expect(number).to eq 2
-      end
-
-      it 'export entry' do
-        vcr('export_entry') do
-          raise NotImplementedError
-        end
-      end
-
-      it 'export asset' do
-        vcr('export_asset') do
-          raise NotImplementedError
-        end
-      end
-
-      it 'export content type' do
-        vcr('export_content_type') do
-          raise NotImplementedError
-        end
-      end
-
-      context 'create_entry_parameters' do
-          raise NotImplementedError
-      end
-
-      context 'parse_attributes_from_hash' do
-          raise NotImplementedError
-      end
-
-      context 'parse_attributes_from_array' do
-          raise NotImplementedError
-      end
     end
   end
 end
